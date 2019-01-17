b7e9380aa9b5e9079df507bf4bd4e8ce0959e419
vats0726 <Vats@4186523N>
2019-01-17 18:42:58 +0530

    Create template-map-hashing.cpp

---
+++ template-map-hashing.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ldb;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+#define db(x) #x<<" "<<(x)<<" "
+#define nl '\n'
+#define PI 3.14159265
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x).size()
+#define mem(a,val) memset(a,val,sizeof(a))
+#define toUpper(x) transform(x.begin(), x.end(), x.begin(),::toupper);
+#define toLower(x) transform(x.begin(), x.end(), x.begin(),::tolower);
+#define deci(n)    cout<<fixed<<setprecision(n)
+#define setbits(x) __builtin_popcountll(x)
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) (a > b ? b : a)
+#define rep(a,b) for(int i=(a);i<=(b);i++)
+
+const int MAX = 1e5+1;
+const ll  INF = 1e18;
+const int MOD = 1e9+7;
+const int dx[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+const int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const int N=1e5+5;
+
+/***************Hashing_Using_Maps*************************/
+map<int,int> om;
+int omele=0;
+void hmap(int ar[],int n)
+{ int cnt=0;
+  rep(0,n-1)
+  {
+      om[ar[i]]++;
+     if(om[ar[i]]==1)
+      omele++;
+  }
+}
+/***********************************************************/
+
+int main()
+{   fastio;
+    int n;
+    cin>>n;
+    int ar[n];
+    rep(0,n-1)
+    {cin>>ar[i];}
+    
+    
+}
